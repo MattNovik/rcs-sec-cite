a792568eacdbdeb24c7f4ddfb5e25bba4fcfdf6e
GOdick <matvej.novik@gmail.com>
2019-06-12 14:15:30 +0300

    add foform, will add css and php(for send)

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
 var slider = Peppermint(document.getElementById('peppermint'));
+var order = document.getElementById('order');
+var form = document.querySelector(".form");
+order.onclick = function () {
+	form.style.visibility = "visible";
+}
