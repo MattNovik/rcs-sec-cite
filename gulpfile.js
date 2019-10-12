abf9a2df3548fc2dac7d7b6147a2f72e37460e89
Godicks <matvej.novik@gmail.com>
2019-10-12 21:58:44 +0300

    start work at pages

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,11 @@ gulp.task("html", function() {
     .pipe(gulp.dest("dist"));
 });
 
+gulp.task("html-documents", function() {
+    return gulp.src("src/html/documents/*.html")
+    .pipe(gulp.dest("dist/html/documents"));
+});
+
 gulp.task("html-to", function() {
     return gulp.src("src/html/to/*.html")
     .pipe(gulp.dest("dist/html/to"));
@@ -53,6 +58,8 @@ gulp.task('browserSync', function() {
   gulp.watch("dist/css/*.css").on('change', browserSync.reload);
   gulp.watch("dist/images/*.+(jpg|jpeg|png|gif|svg)").on('change', browserSync.reload);
   gulp.watch("dist/js/*.js").on('change', browserSync.reload);
+  gulp.watch("dist/html/documеnts/*.html").on('change', browserSync.reload);
+  gulp.watch("dist/html/documеnts/css/*.css").on('change', browserSync.reload);
 });
 
 
@@ -66,6 +73,19 @@ gulp.task('sass', function() { // Создаем таск "sass"
     .pipe(cssnano())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist/css')) // Выгружаем результата в папку css
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass_documents', function() { // Создаем таск "sass"
+  return gulp.src(['src/html/documents/scss/**/*.scss']) // Берем источник
+    .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+    .pipe(autoprefixer({
+       browsers: ['last 2 versions'],
+       cascade: false
+     }))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/html/documents/css')) // Выгружаем результата в папку css
     .pipe(browserSync.stream());
 });
 
@@ -145,9 +165,10 @@ gulp.task("scripts", function() {
 
 gulp.task('watch', function() {
     gulp.watch('src/scss/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('src/html/documents/scss/**/*.scss', gulp.parallel('sass_documents'));
     gulp.watch("src/*.html", gulp.parallel('html'));
     gulp.watch("src/js/*.js", gulp.parallel('scripts'));
-    
+   	gulp.watch("src/html/documents/*.html", gulp.parallel('html-documents'));    
 });
 
-gulp.task("default", gulp.parallel("html","html-to","html-sved","html-obsled","html-cert","html-cert2", "sass","sass_to","sass_sved", "sass_obsled", "sass_cert","sass_cert2","scripts","browserSync", "watch"));
+gulp.task("default", gulp.parallel("html","html-documents","html-to","html-sved","html-obsled","html-cert","html-cert2", "sass","sass_documents","sass_to","sass_sved", "sass_obsled", "sass_cert","sass_cert2","scripts","browserSync", "watch"));
